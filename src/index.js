34c4b55e26f64e5dbc6a1382776fc3a28dd42aa8
Sambit-pradhan2003 <snsambitpradhan@gmail.com>
2024-01-27 14:44:08 +0530

    mongo db connection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-gghfhgf
-kmbmnb,vbnm.m
+import dotenv from "dotenv" 
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+/*const app=Express();
+
+// function connectDB(){}
+
+;(async()=>{
+    try{
+       await mongoose.connect ( `${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",()=>{
+        console.log("err",error);
+        throw error;
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`app is listening on port${process.env.PORT}`);
+       })
+    }
+    catch(error){
+        console.log("error",error)
+        throw error
+    }
+})()*/
