088366f30cdc6226c66459dabafcfcf23020557c
Sambit-pradhan2003 <snsambitpradhan@gmail.com>
2024-01-31 15:29:37 +0530

    debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,8 @@ import {asynchandaler} from "../utils/asynchandaler.js"
 
 const registeruser= asynchandaler(async (req,res)=>{
     res.status(200).json({
-        message:ok
+        message:"sn"
+        
     })
 })
 
