628e4987e24ec463a3f473ac4f98093bb1bd3c02
Sambit-pradhan2003 <snsambitpradhan@gmail.com>
2024-03-19 01:10:59 +0530

    cloudinary bug cleared

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,67 +1,75 @@
 
-import { v2 as cloudinary } from "cloudinary";
+// import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-const uploadoncloudinary = async (file) => {
-  try {
-    const response = await cloudinary.uploader.upload(file, {
-      resource_type: "auto"
-    });
-    return response;
-  } catch (error) {
-    console.error("Error uploading to Cloudinary:", error);
-    return null;
-  }
-};
-
-export { uploadoncloudinary };
-
-
-
-
-
-
-
-
-
-
-
-
-// import { v2 as cloudinary} from "cloudinary";
-// import fs from "fs";
-
-
-          
 // cloudinary.config({ 
 //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
 //   api_key: process.env.CLOUDINARY_API_KEY, 
 //   api_secret: process.env.CLOUDINARY_API_SECRET
 // });
 
-
-// const uploadoncloudinary=async (localfilepath)=>{
-//     try {
-//         if(!localfilepath) return null;
-//     const response=await cloudinary.uploader.upload(localfilepath,{
-//         resource_type:"auto"
-//     })
-//     // console.log("file is uploaded sucessfully",response.url)
-//     if (fs.existsSync(localfilepath)) {
-//         fs.unlinkSync(localfilepath);
-//       }
+// const uploadoncloudinary = async (file) => {
+//   try {
+//     const response = await cloudinary.uploader.upload(file, {
+//       resource_type: "auto"
+//     });
 //     return response;
+//   } catch (error) {
+//     console.error("Error uploading to Cloudinary:", error);
+//     return null;
+//   }
+// };
+
+// export { uploadoncloudinary };
+
+
+
+
+
+
+
+
+
+
+
+
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadoncloudinary=async (localfilepath)=>{
+    try {
+        if(!localfilepath) return null;
+    const response=await cloudinary.uploader.upload(localfilepath,{
+        resource_type:"auto"
+    })
+    // console.log("file is uploaded sucessfully",response.url)
+    if (fs.existsSync(localfilepath)) {
+      fs.unlinkSync(localfilepath);
+      console.log("Local file deleted successfully")
+    }
+    else {
+      console.log("Local file does not exist.");
+  }
+    return response;
     
 
-//     } catch (error) {
-//         fs.unlinkSync(localfilepath)
-//         return null
-//     }
-// }
+    } catch (error) {
+      if (fs.existsSync(localfilepath)) {
+        fs.unlinkSync(localfilepath);
+        console.log("Local file deleted due to error",error);
+    }
+    console.log(error);
+        return null
+    }
+}
 
 
-// export {uploadoncloudinary}
+export {uploadoncloudinary}
